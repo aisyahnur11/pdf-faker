26e8efa58cc81b492f0686d5f2760b4684611325
aisyahnur11 <110007516+aisyahnur11@users.noreply.github.com>
2023-01-27 09:09:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Dompdf\Dompdf;
+$faker = Faker\Factory::create();
+$dompdf = new Dompdf();
+
+$nama=$faker->name();
+$alamat=$faker->text();
+
+$dompdf->loadHtml("
+<table border='1'>
+    <tr>
+    <td>nama</td>
+    <td>alamat</td>
+    </tr>
+
+    <tr>
+    <td>".$nama."</td>
+    <td>".$alamat."</td>
+    </tr>
+</table>
+");
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
